47d7b97ba31c44bca2e9bf2f2e25cbb235ac581f
InfiniteStyles <AntonVS.Professional@gmail.com>
2018-11-19 07:43:51 -0800

    Initial PM-MarketMakers contracts for version 2.0

---
+++ readme.md
@@ -0,0 +1,1 @@
+## Prediction Markets Automated Market Makers (AMM)
---
+++ truffle.js
@@ -0,0 +1,45 @@
+const config = {
+    networks: {
+        mainnet: {
+            host: "localhost",
+            port: 8545,
+            network_id: "1",
+        },
+        ropsten: {
+            host: "localhost",
+            port: 8545,
+            network_id: "3",
+        },
+        kovan: {
+            host: "localhost",
+            port: 8545,
+            network_id: "42",
+        },
+        rinkeby: {
+            host: "localhost",
+            port: 8545,
+            network_id: "4",
+        },
+    },
+    mocha: {
+        enableTimeouts: false,
+        grep: process.env.TEST_GREP
+    }
+}
+
+const _ = require('lodash')
+
+try {
+    _.merge(config, require('./truffle-local'))
+}
+catch(e) {
+    if(e.code === 'MODULE_NOT_FOUND') {
+        // eslint-disable-next-line no-console
+        console.log('No local truffle config found. Using all defaults...')
+    } else {
+        // eslint-disable-next-line no-console
+        console.warn('Tried processing local config but got error:', e)
+    }
+}
+
+module.exports = config
