2b7677d993a519b70337729830459ace176961ab
InfiniteStyles <AntonVS.Professional@gmail.com>
2018-11-25 21:56:05 -0800

    Modifying MarketMaker to be pausable, and fundable with constructor.

--- contracts/LMSRMarketMakerFactory.sol
+++ contracts/LMSRMarketMakerFactory.sol
@@ -3,14 +3,14 @@ pragma solidity ^0.4.24;
 import "./LMSRMarketMaker.sol";
 
 contract LMSRMarketMakerFactory {
-    event LMSRMarketMakerCreation(address indexed creator, LMSRMarketMaker lmsrMarketMaker, PredictionMarketSystem pmSystem, IERC20 collateralToken, bytes32 conditionId, uint64 fee);
+    event LMSRMarketMakerCreation(address indexed creator, LMSRMarketMaker lmsrMarketMaker, PredictionMarketSystem pmSystem, IERC20 collateralToken, bytes32 conditionId, uint64 fee, uint funding);
 
-    function createLMSRMarketMaker(PredictionMarketSystem pmSystem, IERC20 collateralToken, bytes32 conditionId, uint64 fee)
+    function createLMSRMarketMaker(PredictionMarketSystem pmSystem, IERC20 collateralToken, bytes32 conditionId, uint64 fee, uint funding)
         public
         returns (LMSRMarketMaker lmsrMarketMaker)
     {
-        lmsrMarketMaker = new LMSRMarketMaker(pmSystem, collateralToken, conditionId, fee);
+        lmsrMarketMaker = new LMSRMarketMaker(pmSystem, collateralToken, conditionId, fee, funding);
         lmsrMarketMaker.transferOwnership(msg.sender);
-        emit LMSRMarketMakerCreation(msg.sender, lmsrMarketMaker, pmSystem, collateralToken, conditionId, fee);
+        emit LMSRMarketMakerCreation(msg.sender, lmsrMarketMaker, pmSystem, collateralToken, conditionId, fee, funding);
     }
 }
