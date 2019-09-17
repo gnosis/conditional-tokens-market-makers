c51565c1488130c270330d6b5007ab9c213beea2
Alan Lu <alanlu1023@gmail.com>
2019-09-17 14:06:39 -0500

    Simplifying whitelist implementation

--- contracts/Whitelist.sol
+++ contracts/Whitelist.sol
@@ -9,23 +9,19 @@ contract Whitelist is Ownable {
   event UsersAddedToWhitelist(address[] users);
   event UsersRemovedFromWhitelist(address[] users);
 
-  mapping(address => bool) public userWhitelist;
+  mapping(address => bool) public isWhitelisted;
 
   function addToWhitelist(address[] calldata users) onlyOwner external {
     for (uint i = 0; i < users.length; i++) {
-      userWhitelist[users[i]] = true;
+      isWhitelisted[users[i]] = true;
     }
     emit UsersAddedToWhitelist(users);
   }
 
   function removeFromWhitelist(address[] calldata users) onlyOwner external {
     for (uint i = 0; i < users.length; i++) {
-      userWhitelist[users[i]] = false;
+      isWhitelisted[users[i]] = false;
     }
     emit UsersRemovedFromWhitelist(users);
-  }
-
-  function isWhitelisted(address user) view public returns (bool) {
-    return userWhitelist[user];
   }
 }
