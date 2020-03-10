5426a1fb1277776e4c7d18cd8ef2eb4438fbc843
Alan Lu <alanlu1023@gmail.com>
2020-03-10 15:19:59 +0100

    Fix and factor out FixedProductMarketMakerData

--- contracts/FixedProductMarketMaker.sol
+++ contracts/FixedProductMarketMaker.sol
@@ -334,3 +334,53 @@ contract FixedProductMarketMaker is ERC20, ERC1155TokenReceiver {
         emit FPMMSell(msg.sender, returnAmount, feeAmount, outcomeIndex, outcomeTokensToSell);
     }
 }
+
+
+// for proxying purposes
+contract FixedProductMarketMakerData {
+    mapping (address => uint256) internal _balances;
+    mapping (address => mapping (address => uint256)) internal _allowances;
+    uint256 internal _totalSupply;
+
+
+    bytes4 internal constant _INTERFACE_ID_ERC165 = 0x01ffc9a7;
+    mapping(bytes4 => bool) internal _supportedInterfaces;
+
+
+    event FPMMFundingAdded(
+        address indexed funder,
+        uint[] amountsAdded,
+        uint sharesMinted
+    );
+    event FPMMFundingRemoved(
+        address indexed funder,
+        uint[] amountsRemoved,
+        uint collateralRemovedFromFeePool,
+        uint sharesBurnt
+    );
+    event FPMMBuy(
+        address indexed buyer,
+        uint investmentAmount,
+        uint feeAmount,
+        uint indexed outcomeIndex,
+        uint outcomeTokensBought
+    );
+    event FPMMSell(
+        address indexed seller,
+        uint returnAmount,
+        uint feeAmount,
+        uint indexed outcomeIndex,
+        uint outcomeTokensSold
+    );
+    ConditionalTokens internal conditionalTokens;
+    IERC20 internal collateralToken;
+    bytes32[] internal conditionIds;
+    uint internal fee;
+    uint internal feePoolWeight;
+
+    uint[] internal outcomeSlotCounts;
+    bytes32[][] internal collectionIds;
+    uint[] internal positionIds;
+    mapping (address => uint256) internal withdrawnFees;
+    uint internal totalWithdrawnFees;
+}
